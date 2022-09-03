847d7d084fd11db173b7e2664933688cb776971c
Robert Clay <gliderproarc@gmail.com>
2022-09-03 10:05:44 +0900

    adding Javascript... ugg

--- 5.8_genetic_python/genetic.py
+++ 5.8_genetic_python/genetic.py
@@ -1,6 +1,8 @@
 """Genetic algo practice in Python."""
 
-# import functools
+import functools
+import math
+from random import randrange, choice
 from dataclasses import dataclass
 
 
@@ -50,59 +52,122 @@ def assign_shifts(peeps, places):
 
 
 def calculate_fitness(pairs, fitness):
-    """run pairs into the fitness function
+    """Run pairs into the fitness function
     and return the fitness value of the pair."""
-
-    # reduce(pairs, fitness)
-    pass
+    peeps = pairs[0]
+    places = pairs[1]
+    new_pairs = [[x, y] for x in peeps for y in places if x.place == y.name]
+    return functools.reduce(fitness, new_pairs, 0.0)
 
 
 def make_kids(paragon):
     """take the most fit set of pairs from
     last generation and make more kids."""
-
-    # shuffle the order of the places a bit for copies of the paragon
-    pass
+    the_nine = [
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy(),
+        paragon.copy()
+    ]
+    res = map(random_helper, the_nine)
+    return [paragon] + list(res)
 
 
 def generation(pairs, paragon_fitness):
     """Perform one gereration of the genetic algo."""
-
-    # make_kids()
-    # pass the kids to threads
-    # run assign shifts on all the kids
-    # calculate fitness on the resulting pairs
-    # elect a new paragon or keep the old one
-    # return the paragon
-    pass
+    the_kids = make_kids(pairs)
+    paired = list(map(lambda x: assign_shifts(x[0], x[1]), the_kids))
+    fit_nums = list(map(paragon_fitness, paired))
+    zipped = list(zip(fit_nums, paired))
+    zipped.sort()
+    return zipped[0][1]
 
 
-def all_together(peeps, places):
+def all_together(peeps, places, paragon_fitness):
     """Put it al together."""
-
-    # shuffle the order of the places
-    # assign_shifts()
-    # calculte_fitness()
-    # pass the first set of pairs and their fitness score to generation()
-    # run generation for as many times as needed
-    # return final paragagon
-    pass
+    pairs = [peeps, places]
+    for gen in range(0, 1000):
+        pairs = blank_pairs(generation(pairs, paragon_fitness))
+    return assign_shifts(pairs[0], pairs[1])
 
 
-bob = Person("Bob", 5.0, 5.0, "Monday")
+def random_helper(paragon):
+    """Take a paragon and mutate."""
+    rand1 = randrange(0, len(paragon[0]) + 1)
+    rand2 = randrange(0, len(paragon[1]) + 1)
+    choice1 = choice(list(enumerate(paragon[0])))
+    choice2 = choice(list(enumerate(paragon[1])))
+    popped1 = paragon[0].pop(choice1[0])
+    popped2 = paragon[1].pop(choice2[0])
+    paragon[0].insert(rand1, popped1)
+    paragon[1].insert(rand2, popped2)
+    return [paragon[0], paragon[1]]
 
-phil = Person("Phil", 5.0, 5.0, "Tuesday")
 
-loner = Person("Loner", 5.0, 5.0, "Sunday")
+# pass these in to the main call
 
-home = Place("Home", 5.0, 5.0, "Monday")
 
-work = Place("Work", 5.0, 5.0, "Tuesday")
+def fitness_1(pairs):
+    """Distance calc for fitness."""
+    peeps, places = pairs[0], pairs[1]
+    pairs = [[x, y] for x in peeps for y in places if x.place == y.name]
+    return functools.reduce(dist_calc, pairs, 0.0)
 
-prison = Place("Prison", 5.0, 5.0, "Saturday")
 
-my_peeps = [bob, phil, loner]
+def dist_calc(acc, pair):
+    """Calculate the distrance between two points."""
+    x_dif = 2 * (pair[0].x_loc - pair[1].x_loc)
+    y_dif = 2 * (pair[0].y_loc - pair[1].y_loc)
+    tot = abs(x_dif + y_dif)
+    sq = math.sqrt(tot)
+    return acc + sq
 
-my_places = [home, work, prison]
 
-print(assign_shifts(my_peeps, my_places))
+def blank_pairs(pairs):
+    """Set all the places and peeps to none."""
+    old_peeps = pairs[0]
+    old_places = pairs[1]
+    new_peeps = [
+        Person(x.name, x.x_loc, x.y_loc, x.avail_d, "none") for x in old_peeps
+    ]
+    new_places = [
+        Place(x.name, x.x_loc, x.y_loc, x.avail_d, "none") for x in old_places
+    ]
+    return [new_peeps, new_places]
+
+
+def display(pairs):
+    """Pretty print pairs"""
+    peeps = pairs[0]
+    for peep in peeps:
+        print(f'{peep.name}:{peep.place}')
+
+
+bob = Person("Bob", 5.0, 6.0, "Monday")
+
+phil = Person("Phil", 5.0, 7.0, "Tuesday")
+
+loner = Person("Loner", 5.0, 8.0, "Sunday")
+
+far_man = Person("FarMan", 75.0, 75.0, "Monday")
+
+home = Place("Home", 5.0, 9.0, "Monday")
+
+work = Place("Work", 5.0, 10.0, "Tuesday")
+
+prison = Place("Prison", 5.0, 11.0, "Saturday")
+
+cali = Place("Cali", 100.0, 100.0, "Monday")
+
+my_peeps = [far_man, bob, phil, loner]
+
+my_places = [home, work, prison, cali]
+
+winner = all_together(my_peeps, my_places, fitness_1)
+display(winner)
+print(f'Fitness:{fitness_1(winner)}')
